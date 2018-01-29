67596e35e0134d75eb42e652b57edcc531bb7fd7
xenops-lab <xenops.lab@gmail.com>
2018-01-29 18:40:27 +0600

    New Update on 6:40PM 29-Jan-2018

--- js/about.js
+++ js/about.js
@@ -1,43 +1,53 @@
-$(function() {
-    var a = ['1', '2', '3'];
-    var get = [];
-    var remain = [];
-    $('button').on("click", function() {
-        $('.1').removeAttr("id");
-        var myClass = this.className;
-        var button = $("button");
-        for (var i = 0; i < button.length; i++) {
-            if (myClass === a[i]) {
-                get = a[i];
-                $("#tab-" + get).css('opacity', 0).slideDown('slow').animate({ opacity: 1 }, { queue: false, duration: 'slow' });
-            } else {
-                remain += a[i];
-            }
-        }
-        for (var j = 0; j < remain.length; j++) {
-            $("#tab-" + remain[j]).hide();
-        }
-        remain = [];
-    });
-});
+$(function(){
+            
+            var prog= $('.progress');
+            function progress() {
+                var pah = $('.pah');
 
-$(function() {
-    var progress = $('.progress');
-    var pah = $('.pah');
+                for(var j = 0; j <= prog.length; j++) {
+                    var pro = $(prog[j]).data('pro');
 
-    for (var j = 0; j <= progress.length; j++) {
-        var pro = $(progress[j]).data('pro');
+                    $(prog[j]).animate({width: pro + "%"}, 1000);
 
-        $(progress[j]).animate({ width: pro + "%" }, 1000);
-
-        $(pah[j]).each(function() {
-            $(this).prop('Counter', 0).animate({ Counter: pro }, {
-                duration: 1000,
-                easing: 'swing',
-                step: function(now) {
-                    $(this).text(Math.ceil(now));
+                    $(pah[j]).each(function () {
+                        $(this).prop('Counter',0).animate({Counter: pro}, 
+                        {
+                            duration: 1000,
+                            easing: 'swing',
+                            step: function (now) {
+                                $(this).text(Math.ceil(now));
+                            }
+                        });
+                    });
                 }
+            }
+
+            var a = ['1','2','3'];
+            var get = [];
+            var remain = [];
+            $('button').on("click", function(){
+                $('.1').removeAttr("id");
+                var myClass = this.className;
+                var button = $("button");
+                for(var i = 0; i < button.length; i++) {
+                    if(myClass === a[i]) {
+                        get = a[i];
+                        $("#tab-" + get).css('opacity', 0).slideDown('slow').animate(
+                            { opacity: 1 },
+                            { queue: false, duration: 'slow' }
+                        );
+                        if(a[i] == 2) {
+                            progress();
+                        } else {
+                            $(prog).css("width", "0%");
+                        }
+                    } else {
+                        remain += a[i];
+                    }
+                }
+                for(var j = 0; j < remain.length; j++) {
+                    $("#tab-" + remain[j]).hide();
+                }
+                remain = [];
             });
         });
-    }
-});
